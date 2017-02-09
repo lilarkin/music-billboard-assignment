fcc99267fa567769e612695b4dff9a0a7a93ac23
Lindsay Larkin Larkin <lindsay.larkin@gmail.com>
2017-02-08 22:14:34 -0700

    got artist link working and started controller for songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,54 @@
 class SongsController < ApplicationController
+  before_action :set_billboard 
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
   def index
-  end
+   @songs = @billboard.songs 
+ end
 
-  def show
-  end
+ def show
+ end
 
-  def new
-  end
+ def new
+   @song = @billboard.songs.new
+ end
 
-  def edit
-  end
+ def create
+   @song = @billboard.songs.new(song_params)
+   if @song.save
+     redirect_to billboard_song_path(@billboard, @song)
+   else
+   render :new
+   end
+ end
+
+ def edit
+ end
+
+ def update
+   if @song.update(song_params)
+     redirect_to billboard_song_path(@billboard, @song)
+   else
+   render :edit
+   end
+ end
+
+ def destroy
+   @song.destroy
+   redirect_to billboard_song_path(@billboard)
+ end
+
+ private
+#  def set_billboard
+#    @billboard = Billboard.find(params[:billboard_id])
+#  end
+
+ def set_song
+   @artist = @billboard.songs.find(params[:id])
+ end
+
+ def artist_params
+   params.require(:song).permit(:title, :album, :billboard_id)
+ end
 end
+
